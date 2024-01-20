38b64910dc21a8593b4a8bf7985fa2e94817c65d
AlxRdnvc <a.radanovic1984@gmail.com>
2024-01-21 00:02:53 +0100

    added user gateway class and api key authentication

--- api/index.php
+++ api/index.php
@@ -22,9 +22,24 @@ if ($resource != 'tasks') {
     exit;
 }
 
+if (empty($_SERVER["HTTP_X_API_KEY"])) {   
+    http_response_code(400);
+    echo json_encode(["message" => "missing API key"]);
+    exit;
+}
+
+$api_key = $_SERVER["HTTP_X_API_KEY"];
+$database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+$userGateway = new UserGateway($database);
+
+if ($userGateway->getByAPIKey($api_key) === false) {
+    http_response_code(401);
+    echo json_encode(["message" => "invalid API key"]);
+    exit;
+}
+
 header("Content-type: application/json; charset=UTF-8");
 
-$database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
 $taskGateway = new TaskGateway($database);
 $controller = new TaskController($taskGateway);
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
